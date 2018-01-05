f87c6667c8af141836c5472698f61e8380886a8e
Mattiasgrondahl <mattias_grondahl@hotmail.com>
2018-01-05 18:20:49 +0100

    Add files via upload

--- web.py
+++ web.py
@@ -11,9 +11,9 @@ def web():
 	Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 	httpd = SocketServer.TCPServer(("", PORT), Handler)
 	print "serving at port", PORT
-	#httpd.serve_forever()
-	pass
-	test()
+	httpd.serve_forever()
+	#pass
+	#test()
 
 
 def main():
